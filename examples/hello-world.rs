a6f75c8f645c1e12539d080b6a3f94fe5a3a1311
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-03-05 18:01:27 +0100

    Update hello-world.rs

--- examples/hello-world.rs
+++ examples/hello-world.rs
@@ -2,19 +2,15 @@ use may_core::app::context::AppContext;
 use may_core::app::page::Page;
 use may_core::app::MayApp;
 use may_core::config::AppConfig;
-use may_core::layout::{Dimension, Size, Style};
-use may_core::widget::widgets::button::Button;
-use may_core::widget::Widget;
+use may_core::widget::{DummyWidget, Widget};
 
 struct MyPage;
 
 impl Page for MyPage {
-    fn init(&self, ctx: &mut AppContext) {
+    fn init(&self, _: &mut AppContext) {}
 
-    }
-
-    fn render(&self, ctx: &mut AppContext) -> Box<dyn Widget> {
-        Box::new(Button::new())
+    fn render(&self, _: &mut AppContext) -> Box<dyn Widget> {
+        Box::new(DummyWidget::new())
     }
 }
 
