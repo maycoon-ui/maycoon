04be398b39a22d66d0264f64c5e222e572a4c51d
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-06-15 18:40:16 +0200

    Add documentation

--- may-theme/src/globals.rs
+++ may-theme/src/globals.rs
@@ -1,4 +1,8 @@
+/// Global theme values for all widgets to use.
+///
+/// This may be used to invert the text color of widgets that are inside a container.
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
 pub struct Globals {
+    /// Invert text color.
     pub invert_text_color: bool,
 }
