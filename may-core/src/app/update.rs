bc8fdb97ec818d61816bce1345e59c1dd0cbe1ac
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-06-15 18:41:20 +0200

    Reconstruct Update with bitflags crate

--- may-core/src/app/update.rs
+++ may-core/src/app/update.rs
@@ -1,11 +1,22 @@
 use bitflags::bitflags;
 
 bitflags! {
+    /// Update bitflags to define which part of the App should Update.
+    ///
+    /// Possible values:
+    /// - **EVAL** - Re-evaluate the widget tree.
+    /// - **DRAW** - Re-draw the widget tree.
+    /// - **LAYOUT** - Re-layout the widget tree.
+    /// - **FORCE** - Force the App to re-evaluate, re-draw and re-layout the widget tree.
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub struct Update: u8 {
+        /// Re-evaluate the widget tree.
         const EVAL   = 0b00000001;
+        /// Re-draw the widget tree.
         const DRAW   = 0b00000010;
+        /// Re-layout the widget tree.
         const LAYOUT = 0b00000100;
+        /// Force the App to re-evaluate, re-draw and re-layout the widget tree.
         const FORCE  = 0b00001000;
     }
 }
