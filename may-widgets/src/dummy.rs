a9d0a3d765f4bec9b32f539368c62eb6fff6c05c
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-06-26 17:38:40 +0200

    Create dummy.rs

---
+++ may-widgets/src/dummy.rs
@@ -0,0 +1,30 @@
+use may_core::app::info::AppInfo;
+use may_core::app::update::Update;
+use may_core::layout::{LayoutNode, LayoutStyle, StyleNode};
+use may_core::state::State;
+use may_core::vg::Scene;
+use may_core::widget::Widget;
+use may_theme::id::WidgetId;
+use may_theme::theme::Theme;
+
+/// A dummy Widget that does nothing. Useful for testing.
+pub struct DummyWidget;
+
+impl<S: State> Widget<S> for DummyWidget {
+    fn render(&self, _: &mut Scene, _: &mut dyn Theme, _: &AppInfo, _: &LayoutNode) {}
+
+    fn layout_style(&self) -> StyleNode {
+        StyleNode {
+            style: LayoutStyle::default(),
+            children: Vec::new(),
+        }
+    }
+
+    fn update(&mut self, _: &LayoutNode, _: &mut S, _: &AppInfo) -> Update {
+        Update::empty()
+    }
+
+    fn widget_id(&self) -> WidgetId {
+        WidgetId::new("may-widgets", "DummyWidget")
+    }
+}
