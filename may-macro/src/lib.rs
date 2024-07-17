7fcfd88c4f7cd3fc75db421e837a2c8a3cea7b57
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-07-17 17:31:34 +0200

    Add docs

--- may-macro/src/lib.rs
+++ may-macro/src/lib.rs
@@ -1,10 +1,18 @@
+#![warn(missing_docs)]
+
+//! Macros for Maycoon => See `maycoon` crate.
+//!
+//! Contains procedural macros.
+
 mod state;
 
+/// Derives the [State](may_core::state::State) trait for the given struct.
 #[proc_macro_derive(State)]
 pub fn derive_state(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(state::derive_state(proc_macro2::TokenStream::from(input)))
 }
 
+/// Creates a new [StateVal](may_core::state::StateVal) from an expression.
 #[proc_macro]
 pub fn val(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(state::val(proc_macro2::TokenStream::from(input)))
