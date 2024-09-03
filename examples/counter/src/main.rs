f52e26476d0b39263e45385e179c52bce2b66c51
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-09-03 19:52:03 +0200

    Update counter example

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -2,6 +2,7 @@ use maycoon::core::app::update::Update;
 use maycoon::core::app::MayApp;
 use maycoon::core::config::MayConfig;
 use maycoon::core::layout::{AlignItems, Dimension, FlexDirection, LayoutStyle};
+use maycoon::core::state::Val;
 use maycoon::macros::{val, State};
 use maycoon::math::Vector2;
 use maycoon::widgets::button::Button;
@@ -17,23 +18,25 @@ fn main() {
     MayApp::new(MayConfig::default()).run(
         MyState { count: 0 },
         Container::new(vec![
-            Box::new(
+            Val::new_val(Box::new(
                 Button::new(Text::new("Increase".to_string())).with_on_pressed(
                     |state: &mut MyState| {
                         state.count += 1;
                         Update::DRAW
                     },
                 ),
-            ),
-            Box::new(
+            )),
+            Val::new_val(Box::new(
                 Button::new(Text::new("Decrease".to_string())).with_on_pressed(
                     |state: &mut MyState| {
                         state.count -= 1;
                         Update::DRAW
                     },
                 ),
-            ),
-            Box::new(Text::new(val!(|state: &MyState| state.count.to_string()))),
+            )),
+            Val::new_val(Box::new(Text::new(val!(|state: &MyState| state
+                .count
+                .to_string())))),
         ])
         .with_layout_style(LayoutStyle {
             size: Vector2::<Dimension>::new(Dimension::Percent(1.0), Dimension::Percent(1.0)),
