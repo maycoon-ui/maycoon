6d5425ec5095175218cadeac346df562caf41bee
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2024-10-04 14:27:22 +0200

    Fix unused imports

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -1,6 +1,6 @@
 use maycoon::core::app::MayApp;
 use maycoon::core::config::MayConfig;
-use maycoon::macros::{val, State};
+use maycoon::macros::State;
 use maycoon::widgets::text::Text;
 
 #[derive(State)]
