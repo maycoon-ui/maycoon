e0a19fd1c033005c1bbff9793c22f3bee5b84c07
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2025-02-02 17:19:13 +0100

    Restructure macros

--- maycoon-macros/src/lib.rs
+++ maycoon-macros/src/lib.rs
@@ -5,15 +5,16 @@
 //! Contains procedural macros.
 
 mod state;
+mod val;
 
-/// Derives the [`State`](may_core::state::State) trait for the given struct.
+/// Derives the [State](maycoon::core::state::State) trait for the given struct.
 #[proc_macro_derive(State)]
 pub fn derive_state(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(state::derive_state(proc_macro2::TokenStream::from(input)))
 }
 
-/// Creates a new [`StateVal`](may_core::state::Val) from an expression.
+/// Creates a new [Val](maycoon::core::state::Val) from an expression.
 #[proc_macro]
 pub fn val(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    proc_macro::TokenStream::from(state::val(proc_macro2::TokenStream::from(input)))
+    proc_macro::TokenStream::from(val::val(proc_macro2::TokenStream::from(input)))
 }
--- maycoon-macros/src/state.rs
+++ maycoon-macros/src/state.rs
@@ -14,36 +14,3 @@ pub fn derive_state(input: TokenStream) -> TokenStream {
         impl #impl_generics maycoon::core::state::State for #name #ty_generics #where_clause {}
     }
 }
-
-#[inline]
-pub fn val(input: TokenStream) -> TokenStream {
-    let output: TokenStream = if input.is_empty() {
-        // empty val! means empty StateVal
-        quote! {
-            maycoon::core::state::Val::new_val(())
-        }
-    } else if let Ok(expr) = syn::parse2::<Expr>(input.clone()) {
-        match expr {
-            // StateVal from valid closure
-            Expr::Closure(closure) => {
-                quote! {
-                    maycoon::core::state::Val::new_state(#closure)
-                }
-            },
-
-            // Everything else should be a "constant" expression
-            _ => {
-                quote! {
-                    maycoon::core::state::Val::new_val(#expr)
-                }
-            },
-        }
-    } else {
-        // FIXME: Expr::Closure doesn't catch all closures, so we need to handle them here
-        quote! {
-            maycoon::core::state::Val::new_state(#input)
-        }
-    };
-
-    output
-}
