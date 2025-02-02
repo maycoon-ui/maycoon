ceb5b3e3c2bc4f3fbc38b9167a6427eb1069cc81
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2025-02-02 18:30:12 +0100

    Update icon example

--- examples/icon/src/main.rs
+++ examples/icon/src/main.rs
@@ -1,16 +1,11 @@
 use maycoon::core::app::MayApp;
 use maycoon::core::config::MayConfig;
-use maycoon::macros::{val, State};
-use maycoon::widgets::icon::{Icon, SvgIcon};
-
-const ICON_SOURCE: &str = include_str!("../../../assets/logo.svg");
+use maycoon::macros::{svg_icon, State};
+use maycoon::widgets::icon::Icon;
 
 #[derive(State)]
 struct MyState;
 
 fn main() {
-    MayApp::new(MayConfig::default()).run(
-        MyState,
-        Icon::new(val!(SvgIcon::new(ICON_SOURCE).expect("Failed to load icon"))),
-    );
+    MayApp::new(MayConfig::default()).run(MyState, Icon::new(svg_icon!("./assets/logo.svg")));
 }
