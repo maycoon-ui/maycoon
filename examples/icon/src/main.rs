b78074f15daae55818a8580ac1595b4ddbc479cb
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2025-02-04 14:17:55 +0100

    Rewrite Icon Example

--- examples/icon/src/main.rs
+++ examples/icon/src/main.rs
@@ -1,11 +1,14 @@
 use maycoon::core::app::MayApp;
 use maycoon::core::config::MayConfig;
 use maycoon::macros::{svg_icon, State};
+use maycoon::widgets::icon::svg::SvgIcon;
 use maycoon::widgets::icon::Icon;
 
 #[derive(State)]
 struct MyState;
 
 fn main() {
-    MayApp::new(MayConfig::default()).run(MyState, Icon::new(svg_icon!("./assets/logo.svg")));
+    let icon: SvgIcon = svg_icon!("./assets/logo.svg");
+
+    MayApp::new(MayConfig::default()).run(MyState, Icon::new(icon));
 }
