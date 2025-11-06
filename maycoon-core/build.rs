0e4c154ee3aa9531f1d8e7fde3c1e5859091d5fe
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2025-11-06 11:57:44 +0100

    fix:Cargo release-plz maycoon-core

--- maycoon-core/build.rs
+++ maycoon-core/build.rs
@@ -1,4 +1,10 @@
 fn main() {
+    if cfg!(not(feature = "tokio-runner")) {
+        panic!(
+            "No valid task runner feature selected. Please select a `-runner` feature (e.g. `tokio-runner`)."
+        );
+    }
+
     cfg_aliases::cfg_aliases! {
         web: { target_arch = "wasm32" },
         native: { not(target_arch = "wasm32") },
